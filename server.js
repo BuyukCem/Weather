f2b2b8d0a260519f345de5916c4d59f630698542
BuyukCem <cembuyuk7@gmail.com>
2020-05-16 00:24:42 +0200

    change folder structure

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const path = require('path');
 const express = require('express');
-const serv= require('./config/confServeur.js.dist');
-const http=require('http');
+
+require('dotenv').config();
 
 const app = express();
-
-var routes=require('./src/routes/index');
-
-app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'src/public')));
+app.set('views', path.join(__dirname, 'app/public/views'));
+app.use(express.static(path.join(__dirname, 'app/public')));
+
+const routes = require('./src/app/routes/weather');
 app.use('/', routes);
 
 app.use((req, res, next) => {
     res.status(404).render("erreur");
 })
 .use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+     console.error(err.stack);
+     res.status(500).send('Something broke!');
 })
- // console.error(err.stack);
-  //res.status(500).send('Something broke!');
-
-.listen(serv.local.port, () => {
-  console.log(`App running at http://localhost:`+serv.local.port);
+.listen( process.env.SERVER_PORT, () => {
+     console.log('App running at: ' + process.env.SERVER_HOST+":"+ process.env.SERVER_PORT);
 });
