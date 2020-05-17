06f9f8fb369a02e59993666ae0a39997414ba3f1
BUYUK Cem <37942054+BuyukCem@users.noreply.github.com>
2020-05-17 22:34:26 +0200

    Heroku (#1)
    * delete docker
    
    * change folder structure
    
    * change code for heroku

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const path = require('path');
 const express = require('express');
-const serv= require('./config/confServeur.js.dist');
-const http=require('http');
+const port = process.env.PORT || process.env.SERVER_PORT;
+require('dotenv').config();
 
 const app = express();
-
-var routes=require('./src/routes/index');
-
-app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'src/public')));
+app.set('views', path.join(__dirname, 'src/app/public/views'));
+app.use(express.static(path.join(__dirname, 'src/app/public')));
+
+const routes = require('./src/app/routes/weather');
 app.use('/', routes);
 
 app.use((req, res, next) => {
     res.status(404).render("erreur");
 })
 .use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+     console.error(err.stack);
+     res.status(500).send('Something broke!');
 })
- // console.error(err.stack);
-  //res.status(500).send('Something broke!');
-
-.listen(serv.local.port, () => {
-  console.log(`App running at http://localhost:`+serv.local.port);
+.listen(port, () => {
+     console.log('App running at: ' + port);
 });
